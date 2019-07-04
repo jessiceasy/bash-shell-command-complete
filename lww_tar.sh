e0708b06eef2e790b525106133555afaade1a88d
jessiceasy <45992005+jessiceasy@users.noreply.github.com>
2019-07-04 20:24:50 +0800

    Update lww_tar.sh

--- lww_tar.sh
+++ lww_tar.sh
@@ -1,16 +1,17 @@
 _lww_tar() {
     local tips
-    tips[0]='压缩前什么样子解压后就是什么样子，不会自建建目录'
-    tips[1]='tar -czvf ***.tar.gz 要压缩的文件或目录 #gz'
-    tips[2]='tar -cjvf ***.tar.bz2 要压缩的文件或目录 #bz2'
-    tips[3]='tar -xzvf ***.tar.gz  #解压gz'
-    tips[4]='tar -cjvf ***.tar.bz2 #解压bz2'
-    echo tips
-    for i in ${!tips[@]}; do
-        echo i
-     # suggestions[$i]="$(printf '%*s' "-$COLUMNS"  "${suggestions[$i]}")"
+    # 以数字开头，因为bash4.4以下的版本不支持-o nosort输出的时候会自动排序
+    tips[0]='0 压缩前什么样子解压后就是什么样子，不会自建目录'
+    tips[1]='1 tar -czvf ***.tar.gz 要压缩的文件或目录 #gz'
+    tips[2]='2 tar -cjvf ***.tar.bz2 要压缩的文件或目录 #bz2'
+    tips[3]='3 tar -xzvf ***.tar.gz  #解压gz'
+    tips[4]='4 tar -cjvf ***.tar.bz2 #解压bz2'
+    for i in "${!tips[@]}"; do
+      printf -v tips[$i] '%*s' "-$COLUMNS"  "${tips[$i]}"
     done
-    COMPREPLY=( "${tips[@]##*/}" )
-    
+    COMPREPLY=( "${tips[@]}" ) 
 }
-complete -F _lww_tar tar
+complete  -F _lww_tar tar
+# bash4.4以下版本不支持 -o nosort
+# complete -o nosort -F _lww_tar tar 
+
