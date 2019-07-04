7ecb697e12bb55b9bb8ec1a905ce7f7b05408b84
jessiceasy <45992005+jessiceasy@users.noreply.github.com>
2019-07-04 12:46:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,14 @@
-
+# !/bin/sh
+# ./表示当前工作目录，
+# filelist=`ls ./`
+# filelist=`ls ~/bash-shell-command-complete` 不能用~应为如果用sudo执行的话~表示root用户的根目录
+filelist=`ls ./`
+destdir='/etc/bash_completion.d'
+echo $filelist
+for file in $filelist
+do
+ if [ $file != 'README.md' -a $file != 'init.sh' ];then
+ echo 'copying-----'$file
+ cp $file $destdir
+ fi
+done
