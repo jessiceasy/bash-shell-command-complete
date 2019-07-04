2e9dbf0d30e5ed355d05a4af5a62eba9ac579186
jessiceasy <45992005+jessiceasy@users.noreply.github.com>
2019-07-04 17:30:19 +0800

    Create lww_tar.sh

---
+++ lww_tar.sh
@@ -0,0 +1,11 @@
+_lww_tar() {
+    local tips
+    tips[0]='压缩前什么样子解压后就是什么样子，不会自建建目录'
+    tips[1]='tar -czvf ***.tar.gz 要压缩的文件或目录 #gz'
+    tips[2]='tar -cjvf ***.tar.bz2 要压缩的文件或目录 #bz2'
+    tips[3]='tar -xzvf ***.tar.gz  #解压gz'
+    tips[4]='tar -cjvf ***.tar.bz2 #解压bz2'
+    COMPREPLY=( "${tips[@]##*/}" )
+    
+}
+complete -F _lww_tar tar
