f0ecce9212b0f217cebc379333c5c7f25ddd7edc
jessiceasy <45992005+jessiceasy@users.noreply.github.com>
2019-07-05 09:18:49 +0800

    Update lww_tar.sh

--- lww_tar.sh
+++ lww_tar.sh
@@ -2,10 +2,10 @@ _lww_tar() {
     local tips
     # 以数字开头，因为bash4.4以下的版本不支持-o nosort输出的时候会自动排序
     tips[0]='0 压缩前什么样子解压后就是什么样子，不会自建目录'
-    tips[1]='1 tar -czvf ***.tar.gz 要压缩的文件或目录 #gz'
-    tips[2]='2 tar -cjvf ***.tar.bz2 要压缩的文件或目录 #bz2'
-    tips[3]='3 tar -xzvf ***.tar.gz  #解压gz'
-    tips[4]='4 tar -xjvf ***.tar.bz2 #解压bz2'
+    tips[1]='1 -czvf ***.tar.gz 要压缩的文件或目录 #gz'
+    tips[2]='2 -cjvf ***.tar.bz2 要压缩的文件或目录 #bz2'
+    tips[3]='3 -xzvf ***.tar.gz  #解压gz'
+    tips[4]='4 -xjvf ***.tar.bz2 #解压bz2'
     for i in "${!tips[@]}"; do
       printf -v tips[$i] '%*s' "-$COLUMNS"  "${tips[$i]}"
     done
